567ba7224e6b16dd9b46d0935b3468d810d07eab
Rp214365 <patelricky7801@gmail.com>
2024-07-13 20:26:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+/*let score = "33abc";
+let score = null;
+let score = undefined;
+let score = true;
+let score = false;
+let score = "abc";
+
+console.log(typeof(score));
+let valN = Number (score)
+console.log(typeof(valN));
+console.log(valN);*/
+
+// Day 2 Operations
+
+// let value = 10;
+// let negvalue = -value
+// console.log(negvalue);
+
+// let str1 = "Ricky";
+// let str2 = "PAtel";
+// let minestr = str1+str2;
+// console.log(minestr);
+
+
+// console.log(2+true);
+console.log(true+1);
---
+++ index_2.js
@@ -0,0 +1,4 @@
+let str = "44";
+let str_2 = Number(str);
+console.log(str_2);
+console.log(typeof(str_2));1
